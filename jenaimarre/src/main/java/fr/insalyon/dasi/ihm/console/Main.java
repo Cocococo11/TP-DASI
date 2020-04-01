267d91861685a9cbdb10caca62bc41cc02837eaa
Cocococo11 <56036274+Cocococo11@users.noreply.github.com>
2020-04-01 16:00:56 +0200

    tuez moi

--- jenaimarre/src/main/java/fr/insalyon/dasi/ihm/console/Main.java
+++ jenaimarre/src/main/java/fr/insalyon/dasi/ihm/console/Main.java
@@ -46,7 +46,7 @@ public class Main {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("DASI-PU");
         EntityManager em = emf.createEntityManager();
 
-        Personne ada = new Personne("Lovelace", "Ada", "ada", "ada");
+        Personne ada = new Personne("Lovelace", "Ada", "tue", "moi");
         Personne blaise = new Personne("Pascal", "Blaise", "blaise", "blaise");
         Personne fred = new Personne("Fotiadu", "Frédéric", "fred", "fred");
         
