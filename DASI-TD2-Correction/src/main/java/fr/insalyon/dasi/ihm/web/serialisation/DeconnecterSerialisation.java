4adbd3c058f778d8624824edef7bbaac5380df43
Cocococo11 <56036274+Cocococo11@users.noreply.github.com>
2020-05-20 19:33:46 +0200

    inscrire

---
+++ DASI-TD2-Correction/src/main/java/fr/insalyon/dasi/ihm/web/serialisation/DeconnecterSerialisation.java
@@ -0,0 +1,34 @@
+package fr.insalyon.dasi.ihm.web.serialisation;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import fr.insalyon.dasi.metier.modele.Personne;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author B3401
+ */
+public class DeconnecterSerialisation extends Serialisation {
+
+    @Override
+    public void serialiser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        
+        JsonObject container = new JsonObject();
+
+        Boolean connexion = true;
+        container.addProperty("connexion", connexion);
+
+        response.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+        gson.toJson(container, out);
+        out.close();
+    }
+
+}
