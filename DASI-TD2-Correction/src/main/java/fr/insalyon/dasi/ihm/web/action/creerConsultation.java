ae40df6d0f42d2b006cf50e4a0d5cbadec034f69
Cocococo11 <56036274+Cocococo11@users.noreply.github.com>
2020-05-26 14:20:51 +0200

    consultation action

--- DASI-TD2-Correction/src/main/java/fr/insalyon/dasi/ihm/web/action/creerConsultation.java
+++ DASI-TD2-Correction/src/main/java/fr/insalyon/dasi/ihm/web/action/creerConsultation.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fr.insalyon.dasi.ihm.web.action;
+
+import fr.insalyon.dasi.metier.modele.Personne;
+import fr.insalyon.dasi.metier.service.Service;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author TOOOOOOOOP 1
+ * @author DASI Team
  */
-public class creerConsultation {
+public class creerConsultation extends Action {
+
+    @Override
+    public void executer(HttpServletRequest request) {
+        
+        
+    }
     
 }
