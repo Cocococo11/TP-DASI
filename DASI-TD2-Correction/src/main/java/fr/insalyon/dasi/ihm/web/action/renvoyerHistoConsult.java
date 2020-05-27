099880f95f65c4bfac27bd9ffaa9d08b41c30a15
Cocococo11 <56036274+Cocococo11@users.noreply.github.com>
2020-05-27 15:20:09 +0200

    profile historique

--- DASI-TD2-Correction/src/main/java/fr/insalyon/dasi/ihm/web/action/renvoyerHistoConsult.java
+++ DASI-TD2-Correction/src/main/java/fr/insalyon/dasi/ihm/web/action/renvoyerHistoConsult.java
@@ -21,41 +21,16 @@ public class renvoyerHistoConsult extends Action {
    @Override
     public void executer(HttpServletRequest request) {
         
-        String nomedium = (String)request.getParameter("medium");
-        System.out.println("nommedium : "+ nomedium);
         Service service = new Service();
-        Medium medium;
-        
-       switch (nomedium) {
-           case "medium1":
-               medium = service.rechercherMediumId((long)3);
-               System.out.println("Recherche du 1 medium :");
-               break;
-           case "medium2":
-               medium = service.rechercherMediumId((long)1);
-               System.out.println("Recherche du 2 medium :");
-               break;
-           case "medium3":
-               medium = service.rechercherMediumId((long)2);
-               System.out.println("Recherche du 3 medium :");
-               break;
-           default:
-               medium =null;
-               System.out.println("Pas de bon nom medium");
-               break;
-       }
-        
         HttpSession session = request.getSession();
         Long id = (Long)session.getAttribute("idPersonne");
-        Client c = (Client)service.rechercherPersonneParId(id);
-
-        System.out.println("Client :"+c +" et medium: "+medium);
-        
-        request.setAttribute("medium", medium);
-        if(medium!=null)
+        Client client = (Client)service.rechercherPersonneParId(id);
+        service.getHistoriqueConsultations(client);
+      
+        if(client!=null)
         {
             try {
-                service.demanderConsultation(c,medium);
+                request.setAttribute("liste", client.getListeConsultations());
             } catch (Exception ex) {
                 Logger.getLogger(creerConsultation.class.getName()).log(Level.SEVERE, null, ex);
             }
